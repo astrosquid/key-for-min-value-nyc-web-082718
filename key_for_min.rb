10df8ebf538c625398abb3dcbfec75de5fdce47d
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-23 05:41:48 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,12 @@
 
 def key_for_min_value(name_hash)
   smallest = nil
+  small_key = nil
   name.each do |key, val|
-    
+    if val < smallest || smallest == nil 
+      smallest = val 
+      smallest_key = key
+    end 
+  end 
+  
 end
